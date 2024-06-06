f99520cb61e2ed0beac2f9ec9e1ef1e1bf5457d0
Devendra Verma <devendraverma994@gmail.com>
2024-06-06 19:42:58 +0530

    add code in model for negative number and custom delimiter test cases

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -4,6 +4,16 @@ class StringCalculator < ApplicationRecord
 
     delimiter = ','
     num_list = numbers.split(delimiter)
-    num_list.map(&:to_i).sum
+
+    result_array = num_list.flat_map do |element|
+      element.split('"').flat_map do |part|
+        part.split(/\\n|;/)
+      end.map(&:to_i)
+    end
+
+    negatives = result_array.select { |n| n < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    result_array.sum
   end
 end
