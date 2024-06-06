aad6f109df31d8099824f89500b4513d3fd421ef
Devendra Verma <devendraverma994@gmail.com>
2024-06-06 14:18:41 +0530

    add gem for controller testing add test for controller new action

--- spec/requests/string_calculators_spec.rb
+++ spec/requests/string_calculators_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "StringCalculators", type: :request do
+  describe "GET /new" do
+    it "renders the new template" do
+      get new_string_calculator_path
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns @result and @error as nil" do
+      get new_string_calculator_path
+      expect(assigns(:result)).to be_nil
+      expect(assigns(:error)).to be_nil
+    end
+  end
 end
