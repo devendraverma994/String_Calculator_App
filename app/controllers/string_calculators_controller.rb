b663107d991e95f218883cd5990696f0e9a73833
Devendra Verma <devendraverma994@gmail.com>
2024-06-06 19:53:13 +0530

    add code in controller for invalid input

--- app/controllers/string_calculators_controller.rb
+++ app/controllers/string_calculators_controller.rb
@@ -8,6 +8,8 @@ class StringCalculatorsController < ApplicationController
     calculator = StringCalculator.new
     @result = calculator.add(calculator_params[:numbers])
     redirect_to new_string_calculator_path(result: @result)
+  rescue => e
+    redirect_to new_string_calculator_path(error: e.message)
   end
 
   private
