a90a7f05dec57090f9a4841a2e59501ee94920df
Devendra Verma <devendraverma994@gmail.com>
2024-06-06 19:17:28 +0530

    add code for second test in model

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,5 +1,9 @@
 class StringCalculator < ApplicationRecord
   def add(numbers)
     return 0 if numbers.empty?
+
+    delimiter = ','
+    num_list = numbers.split(delimiter)
+    num_list.map(&:to_i).sum
   end
 end
