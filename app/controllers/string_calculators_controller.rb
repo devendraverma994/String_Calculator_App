57bc2068d946e29bc2e687c8d653c8918dd8254b
Devendra Verma <devendraverma994@gmail.com>
2024-06-06 16:13:47 +0530

    add create action in controller

--- app/controllers/string_calculators_controller.rb
+++ app/controllers/string_calculators_controller.rb
@@ -3,4 +3,16 @@ class StringCalculatorsController < ApplicationController
     @result = params[:result]
     @error = params[:error]
   end
+
+  def create
+    calculator = StringCalculator.new
+    @result = calculator.add(calculator_params[:numbers])
+    redirect_to new_string_calculator_path(result: @result)
+  end
+
+  private
+
+  def calculator_params
+    params.require(:calculator).permit(:numbers)
+  end
 end
