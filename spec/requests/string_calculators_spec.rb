e0441b8db912adb76711cd16403c3cc13864e77e
Devendra Verma <devendraverma994@gmail.com>
2024-06-06 16:00:52 +0530

    add test cases for controller create action

--- spec/requests/string_calculators_spec.rb
+++ spec/requests/string_calculators_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe "StringCalculators", type: :request do
       expect(assigns(:error)).to be_nil
     end
   end
+
+  describe "POST /create" do
+    context "with valid input" do
+      it "redirects to new_string_calculator_path with result" do
+        post string_calculators_path, params: { calculator: { numbers: "1,2,3" } }
+        expect(response).to redirect_to(new_string_calculator_path(result: 6))
+      end
+    end
+  end
 end
